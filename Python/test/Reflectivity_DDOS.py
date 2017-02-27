0012145d9997e47bf164dbec0c06ebdb524eee7c
jason.liu <937617839@qq.com>
2017-02-27 05:12:53 -0500

    Reflectivity_DDOS

---
+++ Python/test/Reflectivity_DDOS.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import Queue
+import threading
+import netaddr
+import sys
+from scapy.all import *
+
+ip_address=[]
+for argv in len(sys.argv):
+    if ip == 0:
+        continue
+    ip_address.append(netaddr.IPNetwork(sys.argv[argv]))
+
+scan_target_queue=Queue.Queue()
+for ip_list in ip_address:
+    for ip in ip_list:
+        scan_target_queue.put(ip)
+
+
+def Scan_target():
+    global scan_target_queue
+    targe_ip=scan_target_queue.get()
+    ans,uans=sr(IP(dst=targe_ip)/UDP90/DNS())
+
+
+def reflectivity_ddos():
