7287e7b999bea17ed15daccc3b096437855b5347
jason.liu <937617839@qq.com>
2017-02-28 18:20:17 +0800

    Reflectivity_DDOS

--- Python/test/Reflectivity_DDOS.py
+++ Python/test/Reflectivity_DDOS.py
@@ -18,10 +18,13 @@ for ip_list in ip_address:
         scan_target_queue.put(ip)
 
 
-def Scan_target():
+def Scan_target(target):
     global scan_target_queue
     targe_ip=scan_target_queue.get()
-    ans,uans=sr(IP(dst=targe_ip)/UDP()/DNS())
+    ans,uans=sr(IP(dst=targe_ip)/UDP()/DNS(qd=DNSQR(qname='yahoo.com.',qtype='ALL')))
+    if ans.summary() ==None():
 
+    elif ans.summary()
+    elif ans.summary()
 
 def reflectivity_ddos():
