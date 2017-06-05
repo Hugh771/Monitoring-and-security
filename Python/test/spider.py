d05cab07927989d79196dad329749b21542c5999
LTY <jason771@outlook.com>
2017-06-05 15:27:43 +0800

    spider

--- Python/test/spider.py
+++ Python/test/spider.py
@@ -16,6 +16,7 @@ def spider():
             num+=1
             spidef_img(img.attrs['src'],num)
 
+
 def spidef_img(url_img,img_num):
     if not url_img.startswith('http:'):
         url_img='http:'+url_img
