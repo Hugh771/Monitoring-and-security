2e2411177e0c0668926340190d3ade5a54151287
jason.liu <937617839@qq.com>
2017-08-02 02:41:23 -0400

    fuli

--- Python/test/spider.py
+++ Python/test/spider.py
@@ -8,22 +8,28 @@ import random
 
 def spider():
     num=0
+    page_num=255
     for i in range(0,30):
-        url='http://jandan.net/ooxx/page-'+str(int(2424-num))+'#comments'
-        r=requests.get(url)
+        url='http://jandan.net/ooxx/page-'+str(page_num)+'#comments'
+        page_num -= 1
+        r=requests.get(url,verify=False)
         bs=BeautifulSoup(r.text,'lxml')
         img_list=bs.find_all('img')
         for img in img_list:
-            num+=1
-            spidef_img(img.attrs['src'],num)
+            if img.attrs['src']:
+                num+=1
+                spidef_img(img.attrs['src'],num)
+            else:
+                continue
 
 def spidef_img(url_img,img_num):
     if not url_img.startswith('http:'):
         url_img='http:'+url_img
-    elif url_img.endswith('png'):
-        return 0
+        if not url_img.endswith('jpg'):
+            return 0
+
     print 'img dowlod'
-    urllib.urlretrieve(url_img,'/root/jandanimg/'+str(img_num)+'.jpg')
+    urllib.urlretrieve(url_img,'/root/jandanimg/new/'+str(img_num)+'.jpg')
 
 if __name__ == '__main__':
     spider()
